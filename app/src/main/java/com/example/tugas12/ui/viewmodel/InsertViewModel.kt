84f885371aa58c4d94cf98e21ccd2221ff1af3c2
dinaamlll <dinamaliaa17@gmail.com>
2024-12-29 16:26:10 +0700

    membuat data class InsertUiEvent di InsertViewModel.kt

--- app/src/main/java/com/example/tugas12/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/tugas12/ui/viewmodel/InsertViewModel.kt
@@ -2,6 +2,14 @@ package com.example.tugas12.ui.viewmodel
 
 import com.example.tugas12.model.Mahasiswa
 
+data class InsertUiEvent(
+    val nim:String = "",
+    val nama:String = "",
+    val alamat:String = "",
+    val jenisKelamin:String = "",
+    val kelas:String = "",
+    val angkatan:String = "",
+)
 fun InsertUiEvent.toMhs(): Mahasiswa = Mahasiswa(
     nim = nim,
     nama = nama,
