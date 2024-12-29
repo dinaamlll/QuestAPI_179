dc713d0a8390182fceb00e2ee820c96ac6a38750
dinaamlll <dinamaliaa17@gmail.com>
2024-12-29 17:21:15 +0700

    membuat CostumeTopAppBar utk menampilkan judul halaman dan navigasi kembali

--- app/src/main/java/com/example/tugas12/ui/view/InsertView.kt
+++ app/src/main/java/com/example/tugas12/ui/view/InsertView.kt
@@ -1,11 +1,14 @@
 package com.example.tugas12.ui.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.tugas12.ui.customwidget.CostumeTopAppBar
 import com.example.tugas12.ui.navigation.DestinasiNavigasi
 import com.example.tugas12.ui.viewmodel.InsertViewModel
 
@@ -19,8 +22,23 @@ fun EntryMhsScreen(
     navigateBack: () -> Unit,
     modifier: Modifier = Modifier,
     viewModel: InsertViewModel = viewModel(factory = PenyediaViewModel.Factory)
-){
+) {
     val coroutineScope = rememberCoroutineScope()
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiEntry.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ) { inneePadding ->
+    }
 }
+
+
 
