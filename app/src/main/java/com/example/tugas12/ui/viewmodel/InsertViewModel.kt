a89dce2607c1bf21f83570a4db3f5c216c8b1c24
dinaamlll <dinamaliaa17@gmail.com>
2024-12-29 16:25:19 +0700

    membuat fungsi untuk mengubah InsertUiEvent menjadi Mahasiswa

--- app/src/main/java/com/example/tugas12/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/tugas12/ui/viewmodel/InsertViewModel.kt
@@ -2,6 +2,14 @@ package com.example.tugas12.ui.viewmodel
 
 import com.example.tugas12.model.Mahasiswa
 
+fun InsertUiEvent.toMhs(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
 fun Mahasiswa.toInsertUiEvent():InsertUiEvent = InsertUiEvent(
     nim = nim,
     nama = nama,
