1795062415889c3a4a7fb760081a3a1e31c6679c
dinaamlll <dinamaliaa17@gmail.com>
2024-12-29 16:41:18 +0700

    membuat fungsi getMhs

--- app/src/main/java/com/example/tugas12/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/tugas12/ui/viewmodel/HomeViewModel.kt
@@ -1,6 +1,14 @@
 package com.example.tugas12.ui.viewmodel
 
+import android.net.http.HttpException
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
 import com.example.tugas12.model.Mahasiswa
+import com.example.tugas12.repository.MahasiswaRepository
+import java.io.IOException
+
 
 sealed class HomeUiState{
     data class Success(val mahasiswa: List<Mahasiswa>) : HomeUiState()
@@ -12,5 +20,16 @@ class HomeViewModel(private val mhs: MahasiswaRepository) : ViewModel(){
         private set
     init {
         getMhs()
+    }
+}
+fun getMhs(){
+    viewModelScope.launch {
+        mhsUiState = try {
+            HomeUiState.Success(mhs.getMahasiswa())
+        }catch (e: IOException){
+            HomeUiState.Error
+        }catch (e: HttpException){
+            HomeUiState.Error
+        }
     }
 }
