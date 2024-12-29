b38e0712532214102e6a0bc17c5b9ede1c212057
dinaamlll <dinamaliaa17@gmail.com>
2024-12-29 17:15:48 +0700

    menampilkan tampilan utama untuk layar entri mahasiswa.

--- app/src/main/java/com/example/tugas12/ui/view/InsertView.kt
+++ app/src/main/java/com/example/tugas12/ui/view/InsertView.kt
@@ -1,8 +1,26 @@
 package com.example.tugas12.ui.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.tugas12.ui.navigation.DestinasiNavigasi
+import com.example.tugas12.ui.viewmodel.InsertViewModel
 
 object DestinasiEntry : DestinasiNavigasi { //untuk mendefinisikan rute dan judul yang akan ditampilkan pada tampilan "Entry Mahasiswa"
     override val route = "item_entry"
     override val titleRes = "Entry Mhs"
 }
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntryMhsScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+}
+
