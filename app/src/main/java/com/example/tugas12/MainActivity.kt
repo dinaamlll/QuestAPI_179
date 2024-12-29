dfe8145f19e5c5fa8ad0943fde57b5235d50f146
dinaamlll <dinamaliaa17@gmail.com>
2024-12-29 21:42:31 +0700

    mengubah agar tampilan awalnya MahasiswaApp

--- app/src/main/java/com/example/tugas12/MainActivity.kt
+++ app/src/main/java/com/example/tugas12/MainActivity.kt
@@ -6,12 +6,16 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.tugas12.ui.navigation.PengelolaHalaman
 import com.example.tugas12.ui.theme.Tugas12Theme
+import com.example.tugas12.ui.viewmodel.MahasiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +24,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Tugas12Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    MahasiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
