026cc19da06f08b1eb7acddd09094bea357a05f0
dinaamlll <dinamaliaa17@gmail.com>
2024-12-29 18:41:36 +0700

    menambahkan OnLoading dan OnError composable utk menampilkan indikator loading dan pesan error

--- app/src/main/java/com/example/tugas12/ui/view/HomeView.kt
+++ app/src/main/java/com/example/tugas12/ui/view/HomeView.kt
@@ -1,5 +1,6 @@
 package com.example.tugas12.ui.view
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -8,9 +9,11 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
@@ -20,9 +23,41 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.example.tugas12.R
 import com.example.tugas12.model.Mahasiswa
 
+@Composable
+fun OnLoading(modifier: Modifier = Modifier){
+    Image(
+        modifier = modifier.size(200.dp),
+        painter = painterResource(R.drawable.ic_connection_error),
+        contentDescription = stringResource(R.string.loading)
+    )
+}
+/**
+ * The home screen displaying error message with re-attempt button.
+ */
+
+@Composable
+fun OnError(retryAction:()->Unit, modifier: Modifier = Modifier){
+    Column(
+        modifier=modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_connection_error), contentDescription = ""
+        )
+
+        Text(text = stringResource(R.string.loading_failed), modifier = Modifier.padding(16.dp))
+        Button(onClick = retryAction) {
+            Text(stringResource(R.string.retry))
+        }
+    }
+}
 @Composable
 fun MhsLayout(
     mahasiswa: List<Mahasiswa>,
