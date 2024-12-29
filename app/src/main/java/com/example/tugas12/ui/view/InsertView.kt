3cb5b17516e0124706f069199abdd49be904e460
dinaamlll <dinamaliaa17@gmail.com>
2024-12-29 17:13:37 +0700

    membuat object DestinasiEntry untuk mendefinisikan rute dan judul

---
+++ app/src/main/java/com/example/tugas12/ui/view/InsertView.kt
@@ -0,0 +1,8 @@
+package com.example.tugas12.ui.view
+
+import com.example.tugas12.ui.navigation.DestinasiNavigasi
+
+object DestinasiEntry : DestinasiNavigasi { //untuk mendefinisikan rute dan judul yang akan ditampilkan pada tampilan "Entry Mahasiswa"
+    override val route = "item_entry"
+    override val titleRes = "Entry Mhs"
+}
