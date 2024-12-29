2c18af47b60c2d79abb11cdc2282052a16c2a0fa
dinaamlll <dinamaliaa17@gmail.com>
2024-12-29 20:20:38 +0700

    memperbaiki typo, menambahkan import dan }

--- app/src/main/java/com/example/tugas12/ui/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/tugas12/ui/navigation/PengelolaHalaman.kt
@@ -2,14 +2,17 @@ package com.example.tugas12.ui.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.tugas12.ui.view.DestinasiEntry
 import com.example.tugas12.ui.view.DestinasiHome
+import com.example.tugas12.ui.view.EntryMhsScreen
 import com.example.tugas12.ui.view.HomeScreen
 
 @Composable
-fun PengelolaHalaman(navController.NavHostController = rememberNavController()){
+fun PengelolaHalaman(navController: NavHostController = rememberNavController()) {
     NavHost(
         navController = navController,
         startDestination = DestinasiHome.route,
@@ -32,3 +35,4 @@ fun PengelolaHalaman(navController.NavHostController = rememberNavController()){
             })
         }
     }
+}
