6f0107dbae015abc311172920363aa307c7025b5
dinaamlll <dinamaliaa17@gmail.com>
2024-12-30 11:48:21 +0700

    menambahkan Detail Screen untuk Menampilkan Data Mahasiswa

---
+++ app/src/main/java/com/example/tugas12/ui/view/DetailView.kt
@@ -0,0 +1,155 @@
+package com.example.tugas12.ui.view
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.tugas12.model.Mahasiswa
+import com.example.tugas12.ui.customwidget.CostumeTopAppBar
+import com.example.tugas12.ui.navigation.DestinasiNavigasi
+import com.example.tugas12.ui.viewmodel.PenyediaViewModel
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "item_detail"
+    override val titleRes = "Detail Mhs"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailScreen(
+    nim: String,
+    navigateBack: () -> Unit,
+    onClick: () -> Unit,
+    viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory),
+){
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    val uiState by viewModel.uiState.collectAsState()
+    Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiDetail.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+},
+floatingActionButton = {
+    FloatingActionButton(
+        onClick = onClick,
+        shape = MaterialTheme.shapes.medium,
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Icon(imageVector = Icons.Default.Edit, contentDescription = "Edit Mahasiswa")
+    }
+},
+    ) { innerPadding ->
+        when (uiState) {
+            is DetailUiState.Loading -> {
+                Text("Loading...", Modifier.fillMaxSize())
+            }
+            is DetailUiState.Success -> {
+                val mahasiswa = (uiState as DetailUiState.Success).mahasiswa
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(innerPadding)
+                        .padding(16.dp),
+                    verticalArrangement = Arrangement.spacedBy(12.dp),
+                    horizontalAlignment = Alignment.Start
+                ) {
+                    ItemDetailMhs(mahasiswa = mahasiswa)
+                }
+            }
+            is DetailUiState.Error -> {
+                Text(
+                    text = "Error: Gagal memuat data. Silakan coba lagi.",
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.error
+                )
+            }
+        }
+    }
+}
+    @Composable
+    fun ItemDetailMhs(
+        modifier: Modifier = Modifier,
+        mahasiswa: Mahasiswa
+    ) {
+        Card(
+            modifier = modifier.fillMaxWidth(),
+            colors = CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.surface,
+                contentColor = MaterialTheme.colorScheme.onSurface
+            ),
+            shape = RoundedCornerShape(14.dp),
+            elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+        ) {
+            Column(modifier = Modifier.padding(16.dp)) {
+                ComponentDetailMhs(judul = "NIM", isinya = mahasiswa.nim)
+                Spacer(modifier = Modifier.padding(8.dp))
+
+                ComponentDetailMhs(judul = "Nama", isinya = mahasiswa.nama)
+                Spacer(modifier = Modifier.padding(4.dp))
+
+                ComponentDetailMhs(judul = "Alamat", isinya = mahasiswa.alamat)
+                Spacer(modifier = Modifier.padding(4.dp))
+
+                ComponentDetailMhs(judul = "Jenis Kelamin", isinya = mahasiswa.jenisKelamin)
+                Spacer(modifier = Modifier.padding(4.dp))
+
+                ComponentDetailMhs(judul = "Kelas", isinya = mahasiswa.kelas)
+                Spacer(modifier = Modifier.padding(4.dp))
+
+                ComponentDetailMhs(judul = "Angkatan", isinya = mahasiswa.angkatan)
+            }
+        }
+    }
+
+    @Composable
+    fun ComponentDetailMhs(
+        modifier: Modifier = Modifier,
+        judul: String,
+        isinya: String,
+    ) {
+        Column(
+            horizontalAlignment = Alignment.Start,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(
+                text = "$judul :",
+                style = MaterialTheme.typography.titleMedium.copy(
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold)
+            )
+            Text(
+                text = isinya,
+                style = MaterialTheme.typography.bodyLarge.copy(
+                    fontWeight = FontWeight.Normal,
+                    color = MaterialTheme.colorScheme.onSurface,
+                    fontSize = 18.sp
+                ))
+        }
+    }
+}
